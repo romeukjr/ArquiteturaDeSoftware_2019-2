9c8e5175f71e71596880877a40f4d4b889bff3c7
Romeu Klering Jr <romeukjr@hotmail.com>
2019-11-04 00:49:46 -0300

    Adding server file and Routes module

---
+++ ges-fute/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+export const ROUTES: Routes = [
+  {}
+]
+
+@NgModule({
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
