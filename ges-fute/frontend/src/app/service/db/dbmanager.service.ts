4486e6fad958a3fb86956eb7bed92284834688c7
Bruno <brunofuhr_@hotmail.com>
2019-11-21 21:30:09 -0300

    instancias dos objetos e constantes de rota ficaram faltando

--- ges-fute/frontend/src/app/service/db/dbmanager.service.ts
+++ ges-fute/frontend/src/app/service/db/dbmanager.service.ts
@@ -8,9 +8,8 @@ import { Partida } from '../../model/entity/ingressos/Partida';
 import { IngressoPartida } from '../../model/entity/ingressos/IngressoPartida';
 import { Advogado } from '../../model/entity/juridico/Advogado';
 import { Contrato } from '../../model/entity/juridico/Contrato';
-import { Duvida, Problema, Denuncia } from '../../model/entity/ouvidoria/Ouvidoria';
+import { Chamado } from '../../model/entity/ouvidoria/Ouvidoria';
 import { DBService } from './db.service';
-
 
 
 import constants from '../../util/constants';
@@ -21,24 +20,37 @@ import { Http } from '@angular/http';
 })
 export class DBManagerService {
 
-  public AlimentoPlano: DBService<AlimentoPlano>;
-  public AvaliacaoDesempenho: DBService<AvaliacaoMedica>;
-  public AvaliacaoMedica: DBService<AvaliacaoDesempenho>;
-  public PlanoAlimentar: DBService<PlanoAlimentar>;
-  public Atleta: DBService<Atleta>;
+  public alimentoPlano: DBService<AlimentoPlano>;
+  public avaliacaoDesempenho: DBService<AvaliacaoMedica>;
+  public avaliacaoMedica: DBService<AvaliacaoDesempenho>;
+  public planoAlimentar: DBService<PlanoAlimentar>;
+  public atleta: DBService<Atleta>;
   
-  public Usuario: DBService<Usuario>;
+  public usuario: DBService<Usuario>;
   
-  public Partida: DBService<Partida>;
-  public IngressoPartida: DBService<IngressoPartida>;
+  public partida: DBService<Partida>;
+  public ingressoPartida: DBService<IngressoPartida>;
 
-  public Advogado: DBService<Advogado>;
-  public Contrato: DBService<Contrato>;
+  public advogado: DBService<Advogado>;
+  public contrato: DBService<Contrato>;
 
-  public Duvida: DBService<Duvida>;
-  public Problema: DBService<Problema>;
-  public Denuncia: DBService<Denuncia>;
+  public chamado: DBService<Chamado>;
 
   constructor(private http: Http) {
+    this.alimentoPlano = new DBService<AlimentoPlano>(http, constants.API.ALIMENTO_PLANO);
+    this.avaliacaoDesempenho = new DBService<AvaliacaoMedica>(http, constants.API.AVALIACAO_DESEMPENHO);
+    this.avaliacaoMedica = new DBService<AvaliacaoDesempenho>(http, constants.API.AVALIACAO_MEDICA);
+    this.planoAlimentar = new DBService<PlanoAlimentar>(http, constants.API.PLANO_ALIMENTO);
+    this.atleta = new DBService<Atleta>(http, constants.API.ATLETA);
+    
+    this.usuario = new DBService<Usuario>(http, constants.API.USUARIO);
+    
+    this.partida = new DBService<Partida>(http, constants.API.PARTIDA);
+    this.ingressoPartida = new DBService<IngressoPartida>(http, constants.API.INGRESSO_PARTIDA);
+
+    this.advogado = new DBService<Advogado>(http, constants.API.ADVOGADO);
+    this.contrato = new DBService<Contrato>(http, constants.API.CONTRATO);
+
+    this.chamado = new DBService<Chamado>(http, constants.API.OUVIDORIA);
   }
 }
